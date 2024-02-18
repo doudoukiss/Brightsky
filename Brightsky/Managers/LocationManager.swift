46ef6610a1119cdd9a21feac75bc8a5e4bf8ef55
doudoukiss <rayallen1996@gmail.com>
2024-02-18 16:50:03 +0900

    Initial commit

---
+++ Brightsky/Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Brightsky
+//
+//  Created by Seattle on 2024/2/18.
+//
+
+import Foundation
---
+++ Brightsky/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Brightsky
+//
+//  Created by Seattle on 2024/2/18.
+//
+
+import Foundation
